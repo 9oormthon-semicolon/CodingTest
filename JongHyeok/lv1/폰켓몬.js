f8314b0133ed00269d1e07bfb1286a1ca6c6732c
deenuit113 <leejh5314@gmail.com>
2024-08-26 14:52:39 +0900

    240826 lv1 폰켓몬

--- JongHyeok/lv1/폰켓몬.js
+++ JongHyeok/lv1/폰켓몬.js
@@ -1,17 +1,7 @@
 function solution(nums) {
-    var answer = [];
-    var max = nums.length / 2;
-        
-    for(let i = 0; i < nums.length; i++) {
-        if(answer.length < max) {
-            if(!answer.includes(nums[i])) {
-                answer.push(nums[i]);
-            }
-        } else if (answer.length === max) {
-            break;
-        }
-    }
-    return answer.length;    
+    const max = nums.length / 2;
+    const arr = [...new Set(nums)];
+    return arr.length > max ? max : arr.length
 }
 
 const nums = [3,1,2,3];
@@ -19,7 +9,9 @@ console.log(solution(nums));
 
 /* (nums 배열의 길이 / 2)만큼 포켓몬을 고를 수 있습니다.
 최대한 많은 종류의 포켓몬을 골라야 합니다.
-그러기 위해서는 배열을 순회하며 새로운 종류의 포켓몬이 있다면,
-정답(내가 가진 포켓몬의 종류 수)에 추가합니다.
-(단, 내가 가진 포켓몬의 종류 수를 초과한다면 더는 추가할 수 없습니다.)
-시간복잡도: O(n)*/
+set로 중복 포켓몬 없앱니다. > 포켓몬의 종류만 남습니다.
+전체 포켓몬의 종류와 고를 수 있는 포켓몬의 종류 둘 중 작은 수가 정답입니다.
+배열 > Set의 시간복잡도: O(n)*/
+
+/*문제 카테고리가 해시이기 떄문에 답을 바꿨습니다...
+  전에 쓴 답은 시간 복잡도도 O(n^2)였습니다...*/
