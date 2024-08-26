406b328a7876f290efb575d18d936f98b4980b3b
ph0109 <132151797+ph0109@users.noreply.github.com>
2024-08-26 14:53:07 +0900

    Update 폰켓몬.js

--- HyeJeong/lv1/폰켓몬.js
+++ HyeJeong/lv1/폰켓몬.js
@@ -1,1 +1,13 @@
-
+function solution(nums) {
+  //Set객체를 사용해서 폰켓몬의 종류를 파악함
+    var answer = new Set(nums);
+    var number = nums.length / 2;
+  //폰켓몬의 종류가 number보다 크다면 폰켓몬의 개수보다 크기 때문에 number를 반환 
+    if(number<answer.size){
+        return number;
+    }
+  //그렇지 않다면 폰켓몬의 종류를 반환
+    else{
+        return answer.size;
+    }
+}
