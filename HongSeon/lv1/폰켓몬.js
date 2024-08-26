bdf34581376a7d4a809baeb87d0ced96f2a4dcd1
felix <fahn0719@gmail.com>
2024-08-26 16:24:24 +0900

    240826 폰켓몬 시간복잡도 설명 추가

--- HongSeon/lv1/폰켓몬.js
+++ HongSeon/lv1/폰켓몬.js
@@ -13,3 +13,22 @@ function solution(nums) {
     // 삼항 연산자 이용하여 더 작은 값 반환
     return beforeArrLen < afterArrLen ? beforeArrLen : afterArrLen
 }
+
+/* 
+시간 복잡도 계산
+
+1. const beforeArrLen = nums.length / 2
+- 상수 시간 => 시간 복잡도 O(1)
+
+2. const afterArr = [...new Set(nums)]
+- 배열 모든 요소 순회하면서 배열을 Set으로 변환 수행 => 시간 복잡도 O(n)
+- Set을 다시 배열로 변환 (위와 같이 모든 요소 순회) => 시간 복잡도 O(n)
+
+3. const afterArrLen = afterArr.length
+- 상수 시간 => 시간 복잡도 O(1)
+
+4. beforeArrLen < afterArrLen ? beforeArrLen : afterArrLen
+- 상수 시간 => 시간 복잡도 O(1)
+
+코드 전체 시간 복잡도 => O(n)
+*/
