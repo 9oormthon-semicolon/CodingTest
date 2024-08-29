72a11bf30ce6f69d0e033a7db6f2463bc414b3c3
ph0109 <132151797+ph0109@users.noreply.github.com>
2024-08-29 14:16:09 +0900

    Create 서울에서 김서방 찾기.js

---
+++ HyeJeong/lv1/서울에서 김서방 찾기.js
@@ -0,0 +1,8 @@
+function solution(seoul) {
+    const index = seoul.indexOf("Kim");
+    return `김서방은 ${index}에 있다`;
+}
+
+//indexOf 메서드를 사용해서 seoul배열에서 Kim 인덱스를 찾음
+//결과 문자열 반환
+//시간복잡도 - indexOf : O(n)
