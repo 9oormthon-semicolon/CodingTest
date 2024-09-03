68e8428710fe8a3070e4fa283e32085c97898618
ki2183 <ki2183@naver.com>
2024-09-03 15:21:51 +0900

    24 09 03 문제 수정 실패율

--- GiJoon/lv1/실패율.js
+++ GiJoon/lv1/실패율.js
@@ -34,3 +34,37 @@ function solution(N, stages) {
 //arr.sort().map --- O(log n)*O(N)
 // ------------------------------------
 // O((N + M) log n) = O(N log n)
+
+//예전에 풀었던거 똑같이 풀어서 양심에 찔렸습니다.
+//아래는 객체를 이용한 풀이입니다
+
+//N은 전체스테이지 개수 멈춰있는 플레이어 스테이지
+function solution(N, stages) {
+    
+    const records = stages.reduce((obj,v) => {
+       obj[v] = (obj[v]||0) + 1
+       return obj
+    }, {})
+       
+    let acc = 0
+    let arr = []
+    const len = stages.length
+    
+    for (let i = 1; i <= N; i++) {
+        // 현재 스테이지에 머물러 있는 플레이어들
+        const players = records[i]||0;
+        // 깬 플레이어 수 + 현재 스테이지 머물러
+        const total = len - acc;
+        // 실패율 계산 토탈이 0 이면 성공률 0
+        const failRate = total === 0 ? 0 : players / total;
+        arr.push([failRate, i]);
+        // 누적 플레이어 수 업데이트
+        acc += players
+    }
+    
+    //정렬 같으면 스테이지 오름 차순
+    arr.sort((a, b) => b[0] - a[0] || a[1] - b[1])
+
+    return arr.map(([_,idx]) => idx)
+}
+
