449dd38c75f855948e10ff7572ae07f5bfcac52a
deenuit113 <leejh5314@gmail.com>
2024-09-04 14:28:24 +0900

    240904 lv1 3진법 뒤집기, 완주하지 못한 선수

--- JongHyeok/lv1/완주하지못한선수.js
+++ JongHyeok/lv1/완주하지못한선수.js
@@ -1,12 +1,22 @@
 function solution(participant, completion) {
-    let combination = participant.concat(completion); 
-    let filtered = combination.filter(item => !participant.includes(item) || !completion.includes(item));
-    let result = filtered.join('');
-    return result;
-}
+    let hash = []
+    participant.forEach(entry => {
+        hash[entry] = hash[entry] ? hash[entry] + 1 : 1 // 해당 이름이 이미 존재하면 + 1, 없었으면 0 > 1
+    })
+    completion.forEach(entry => {
+        hash[entry] = hash[entry] - 1 // 완주한 선수의 이름으로 value - 1
+    })
 
+    for (var key in hash) {
+        if (hash[key] >= 1) return key
+    }
+}
 /*
- participant와 completion을 합칩니다. concat 시간복잡도 O(n)
- combination 배열에서 participant와 completion에 모두 포함되지 않는 요소를 찾습니다. filter 시간복잡도 O(n)
- 필터링된 요소를 문자열로 합칩니다. join 시간복잡도 O(n)
- */
+해시 사용
+participant 배열에는 선수들의 이름이 들어있다
+선수의 이름을 key로 이름이 나온 횟수를 value로 한다
+completion 배열을 순회하면서 완주한 선수 이름의 value 값을 뺀다.
+key 값을 다시 해시를 탐색해서 value가 1이상인 선수의 이름(key)를 리턴한다.
+
+forEach 와 for 문을 사용했기 때문에 시간복잡도 O(n)
+*/
