e52bbfa869e49748c7602416640182c5f5546362
felix <fahn0719@gmail.com>
2024-09-13 15:53:43 +0900

    다른분들 코드 보고 짝지어제거하기 코드 추가

--- HongSeon/lv2/짝지어제거하기.js
+++ HongSeon/lv2/짝지어제거하기.js
@@ -3,7 +3,9 @@ function solution(s) {
     let stack = [];  
     for (let i of s) {
         // 스택의 마지막 값이 현재 값과 같다면 pop 아니라면 push
-        (stack[stack.length - 1] === i) ? stack.pop() : stack.push(i)
+        // stack.length > 0를 안해주니까 효율성 테스트케이스2를 통과 못하는 경우가 발생
+        // 근데 안해줘도 통과하는 경우도 다수 발생 (뭐지..?)
+        (stack[stack.length - 1] === i && stack.length>0) ? stack.pop() : stack.push(i)
     }
     
     return stack.length === 0 ? 1 : 0;
