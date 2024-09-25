afe9cba7bcfb4140ca27a376e69cdc7c59923d2c
deenuit113 <leejh5314@gmail.com>
2024-09-25 14:55:11 +0900

    240925 lv1 성격 유형 검사하기, lv2 구명보트

--- JongHyeok/lv2/구명보트.js
+++ JongHyeok/lv2/구명보트.js
@@ -1,23 +1,25 @@
 function solution(people, limit) {
     let answer = 0;
     let left = 0; // 가장 가벼운 사람
-    let right = people.length - 1; // 가장 무거운 사람
+     // 가장 무거운 사람
      
     // 몸무게 정렬
     // 자바스크립트 TimSort는 O(nlogn)의 시간복잡도
     people.sort((a, b) => a - b);
     
-    while (left <= right) { // 시간복잡도 O(n) [모든 보트에 1명만 타는 경우]
+    while (people.length) { // 시간복잡도 O(n) [모든 보트에 1명만 타는 경우]
         // 가벼운 사람 + 가장 무거운 사람 / 무게가 limit을 초과하기 전까지 가벼운 사람 태움
+        let right = people.length - 1;
         if (people[left] + people[right] <= limit) {
-            left++;
-            right--;
+            people.shift();
+            people.pop();
+            answer++;
         } 
         // 가벼운 사람이 타지 못 하는 경우
         else {
-            right--;
+            people.pop();
+            answer++;
         }
-        answer++;
     }
     return answer;
 }
