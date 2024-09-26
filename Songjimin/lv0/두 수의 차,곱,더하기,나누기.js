e91b08f5b1aaf231a9d8443e6af100c1e048c99d
usessss <89915413+usessss@users.noreply.github.com>
2024-09-26 15:24:25 +0900

    Create 두 수의 차,곱,더하기,나누기.js

---
+++ Songjimin/lv0/두 수의 차,곱,더하기,나누기.js
@@ -0,0 +1,4 @@
+const solution  = (num1, num2) => num1 - num2
+const solution  = (num1, num2) => num1 * num2
+const solution  = (num1, num2) => num1 / num2
+const solution  = (num1, num2) => num1 + num2
