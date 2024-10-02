ce3daa36dcf19ccc2118671bfc2bf446f0d31403
deenuit113 <leejh5314@gmail.com>
2024-10-02 15:29:07 +0900

    241002 lv2 숫자의 표현, [1차] 캐시

--- JongHyeok/lv2/숫자의표현.js
+++ JongHyeok/lv2/숫자의표현.js
@@ -1,15 +1,37 @@
 function solution(n) {
     let answer = 0;
-    for(let i = 0; i <= n; i++) {
-        if(n % i === 0 && i % 2 === 1) answer++;
+    for(let i = 1; i <= n / 2; i++) {
+        let sum = 0;
+        for (let j = i; j <= n; j++) {
+            sum += j;
+            if(sum === n) {
+                answer++;
+                break;
+            }
+            if (sum > n) break;
+        }
     }
-    return answer;
-  }
+    return answer + 1;
+}
+/*
+처음에 단순하게 풀었던 반복문 2개를 최적화시켜보았습니다.
+연속된 수로 이루어져 있을 때 (자기자신 하나만 있을 때 제외하고 answer+1)
+절반까지만 탐색하면 됩니다.
+시간복잡도 n^2로 동일하긴 하지만
+n/2로 줄인 게 효율성 테스트에 도움이 됐네요.
 
 console.log(solution(18));
 
 /*
 주어진 수를 연속된 자연수의 합으로 표현하는 방법의 수는 주어진 수의 홀수 약수 개수와 같습니다.
+
+function solution(n) {
+    let answer = 0;
+    for(let i = 0; i <= n; i++) {
+        if(n % i === 0 && i % 2 === 1) answer++;
+    }
+    return answer;
+  }
 
 ex) n이 20일 때: 1 2 4 5 10 20 > 1 5
 약수가 1 > 연속하는 1개 자연수의 합으로 표현 가능
