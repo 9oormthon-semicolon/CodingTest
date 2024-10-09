a05c04f10fa282c073c2c982bc80da316fefb2b7
deenuit113 <leejh5314@gmail.com>
2024-10-09 22:00:31 +0900

    241009 lv2 주식가격

---
+++ JongHyeok/lv2/주식가격.js
@@ -0,0 +1,27 @@
+function solution(prices) {
+    const answer = [];
+    const stack = [];
+
+    for (let i = 0; i < prices.length; i++) {
+        while (stack.length > 0 && prices[i] < prices[stack[stack.length - 1]]) {
+            // 현재 가격이 스택의 맨 위에 있는 시점의 가격보다 낮을 때
+            const sec = stack.pop(); // 가격이 떨어진 시점
+            answer[sec] = i - sec; // 걸린 시간
+        }
+        stack.push(i);
+    }
+
+    while (stack.length > 0) { // 끝까지 가격이 떨어지지 않은 경우
+        const sec = stack.pop(); 
+        answer[sec] = (prices.length - 1) - sec; // 그 가격이 추가된 시점부터 끝까지 시간
+    }
+
+    return answer;
+}
+
+const prices = [1,2,3,2,3];
+console.log(solution(prices));
+
+/*
+시간복잡도 O(n)
+*/
