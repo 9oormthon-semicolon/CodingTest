702d53262d676b2f33afd9e399031966f972be74
deenuit113 <leejh5314@gmail.com>
2024-10-23 14:59:27 +0900

    241023 lv2 괄호 변환

---
+++ JongHyeok/lv2/괄호변환.js
@@ -0,0 +1,42 @@
+function solution(p) {
+    function isRightStr(str) { // 올바른 문자열인지
+        let total = 0; // '('와 ')'의 차이
+        for (const char of str) {
+            if (char === '(') total++; 
+            else if (char === ')') total--;
+            
+            if (total < 0) return false;
+        }
+        return true;
+    }
+
+    function splitBalancedStr(str) {
+        let total = 0; // '('와 ')'의 차이
+        for (let i = 0; i < str.length; i++) {
+            if (str[i] === '(') total++; 
+            else if (str[i] === ')') total--;
+
+            if (total === 0) return [str.slice(0, i + 1), str.slice(i + 1)]; // 문자열 분리
+        }
+    }
+
+    function fixStr(str) {
+        if (str === "") return ""; // 입력이 빈 문자열인 경우, 빈 문자열 반환
+        const [u, v] = splitBalancedStr(str); // "균형잡힌 괄호 문자열" u, v로 분리
+
+        if (isRightStr(u)) return u + fixStr(v);
+        // u가 "올바른 괄호 문자열"이라면 문자열을 u에 이어 붙인 후 반환
+        else return `(${fixStr(v)})` +
+                    Array.from(u.slice(1, -1), c => c === '(' ? ')' : '(').join('');
+        /* 교정된 v 앞 뒤로 '(' 와 ')' 추가한 새로운 문자열 +
+         u의 첫 번째와 마지막 문자 제거(slice), 나머지 문자들을 반대로 바꾼 문자열
+        */
+    }
+
+    if (isRightStr(p)) return p; // 만약 p가 이미 "올바른 괄호 문자열"이라면 그대로 반환
+
+    return fixStr(p);
+}
+
+const p = "()))((()";
+console.log(solution(p)); // "()(())()"
